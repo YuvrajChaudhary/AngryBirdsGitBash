cf4ce543af7f749daddb59afdc5f652316c9aa22
unknown <yc7913882@gmail.com>
2020-10-22 16:52:06 +0530

    i have added sketch.js chain.js index.html

---
+++ Chain.js
@@ -0,0 +1,27 @@
+class Chain {
+        constructor(bodyA, bodyB) {
+            var options = {
+                bodyA : bodyA,
+                bodyB : bodyB,
+                lenght : 20,
+                stiffness: 1.05
+            }
+        
+            this.chain = Constraint.create(options);
+            World.add(world, this.chain);
+
+
+
+
+
+        }
+        display() {
+            line(this.chain.bodyA.position.x , this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y);
+
+        } 
+
+
+
+
+
+}
